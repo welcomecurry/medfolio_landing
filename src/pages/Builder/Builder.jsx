3a80bb1cc29e4c4be92ab30bea6d5162ddc5c4a0
welcomecurry <bhandalamit@gmail.com>
2023-03-30 09:39:36 -0600

    visualize info from api

--- src/pages/Builder/Builder.jsx
+++ src/pages/Builder/Builder.jsx
@@ -1,21 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import { storage } from "../../firebase";
 import axios from "axios";
-import './Builder.css';
-import { parseResume } from '../../resumeAPI';
-import Selector from '../../components/Selector.jsx/Selector';
+import "./Builder.css";
+import { parseResume } from "../../resumeAPI";
+import Selector from "../../components/Selector.jsx/Selector";
+import { BuildLoader } from "../../components/BuildLoader/BuildLoader";
 
 function Builder() {
-  const [fileContent, setFileContent] = useState('');
+  const [fileContent, setFileContent] = useState("");
   const [parsedData, setParsedData] = useState(null);
+  const [fileUploaded, setFileUploaded] = useState(false);
+
+  const [url, setUrl] = useState(null);
 
   const [progress, setProgress] = useState(0);
 
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
+    setFileUploaded(true)
     uploadFiles(file);
-  }
+  };
 
   const uploadFiles = (file) => {
     //
@@ -35,41 +40,72 @@ function Builder() {
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
           console.log("File available at", downloadURL);
-          parseResume(downloadURL)
+          const fetchAPI = async () => {
+            setParsedData(await parseResume(downloadURL));
+            console.log(parsedData);
+            
+          };
+
+          fetchAPI();
         });
       }
     );
   };
 
+  useEffect(() => {
+    console.log(parsedData);
+  }, [parsedData]);
+
   return (
     <div className="file-uploader-container">
-        <link
-            href="https://cdn.jsdelivr.net/npm/remixicon@2.2.0/fonts/remixicon.css"
-            rel="stylesheet"
-        />
+      <link
+        href="https://cdn.jsdelivr.net/npm/remixicon@2.2.0/fonts/remixicon.css"
+        rel="stylesheet"
+      />
       <div className="upload-section">
         <h1>Upload Resume/CV</h1>
-        <div style={{padding: "1rem"}}>
+        <div style={{ padding: "1rem" }}>
           <label class="button button--flex">
-            <input type="file" onChange={handleFileUpload}/>
-            Select  <i className="ri-upload-line" />
+            <input type="file" onChange={handleFileUpload} />
+            Select <i className="ri-upload-line" />
           </label>
         </div>
 
         <div>
-            <p>Or</p>
-            <a href="" className="button button--flex">
+          <p>Or</p>
+          <a href="" className="button button--flex">
             Connect to LinkedIn <i className="ri-linkedin-box-fill" />
-        </a>
+          </a>
 
-        <Selector></Selector>
+          <Selector></Selector>
         </div>
       </div>
       <div className="">
-        <p>Your portfolio created by artificial intelligence will appear here.</p>
-        {fileContent && (
-          <pre className="">{fileContent}</pre>
+        <p>
+          Your portfolio created by artificial intelligence will appear here.
+        </p>
+        {parsedData && (
+          <div>
+            {Object.entries(parsedData).map(([sectionName, sectionData]) => (
+              <div key={sectionName}>
+                <h2>{sectionName}</h2>
+                {Object.entries(sectionData).map(([fieldName, fieldValue]) => (
+                  <div key={fieldName}>
+                    <p>
+                      {fieldName}:{" "}
+                      {typeof fieldValue === "object"
+                        ? JSON.stringify(fieldValue)
+                        : fieldValue}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
         )}
+
+        {!parsedData && fileUploaded && (
+          <><BuildLoader /><p>Building...</p></>)}
       </div>
     </div>
   );
--- src/resumeAPI.js
+++ src/resumeAPI.js
@@ -1,25 +1,27 @@
-export function parseResume(file) {
-    console.log(file)
-    console.log(file.file)
-
-const options = {
-    method: 'POST',
-    headers: {
-      accept: 'application/json',
-      'content-type': 'application/json',
-      authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTc5MGMxMTktMzVkOS00MGEyLTk1YzItNjFhZGI2NjYyMzJjIiwidHlwZSI6ImFwaV90b2tlbiJ9.aCP6lXwnaX5JSqL23cMLjF37bWiVNlggd_L1vwfbS44'
-    },
-    body: JSON.stringify({
-      response_as_dict: true,
-      attributes_as_list: false,
-      show_original_response: false,
-      providers: 'affinda',
-      file_url: file
-    })
+export const parseResume = async (file) => {
+    const options = {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+        authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTc5MGMxMTktMzVkOS00MGEyLTk1YzItNjFhZGI2NjYyMzJjIiwidHlwZSI6ImFwaV90b2tlbiJ9.aCP6lXwnaX5JSqL23cMLjF37bWiVNlggd_L1vwfbS44'
+      },
+      body: JSON.stringify({
+        response_as_dict: true,
+        attributes_as_list: false,
+        show_original_response: false,
+        providers: 'affinda',
+        file_url: file
+      })
+    };
+  
+    try {
+      const response = await fetch('https://api.edenai.run/v2/ocr/resume_parser', options);
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
   };
   
-  fetch('https://api.edenai.run/v2/ocr/resume_parser', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-}
